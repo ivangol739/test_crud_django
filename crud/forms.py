b7509ae4f3f8cca67524971a78270cb37979a6b7
ivangol739 <ivangol739@gmail.com>
2025-01-20 22:39:45 +0300

    valid

--- crud/forms.py
+++ crud/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(label='Имя', initial='undefined', help_text='Введите свое имя')
-    age = forms.IntegerField(label='Ваш возраст?', widget=forms.Textarea, initial=18, help_text='Введите свой возраст')
+    name = forms.CharField(label='Имя', initial='undefined', help_text='Введите свое имя', min_length=2, max_length=10)
+    age = forms.IntegerField(label='Ваш возраст?', widget=forms.Textarea, initial=18, help_text='Введите свой возраст', min_value=1, max_value=100)
+    reklama = forms.BooleanField(label='Согласны получать рекламу?', required=False)
--- crud/views.py
+++ crud/views.py
@@ -3,11 +3,24 @@ from django.shortcuts import render
 from .forms import UserForm
 
 
+# def index(request):
+#     if request.method == "POST":
+#         name = request.POST.get("name")
+#         age = request.POST.get("age")
+#         return HttpResponse(f"<h2>Привет, {name}, твой возрас: {age}</h2>")
+#     else:
+#         userform = UserForm()
+#         return render(request, "crud/index.html", {"form": userform})
+
+
 def index(request):
     if request.method == "POST":
-        name = request.POST.get("name")
-        age = request.POST.get("age")
-        return HttpResponse(f"<h2>Привет, {name}, твой возрас: {age}</h2>")
+        userform = UserForm(request.POST)
+        if userform.is_valid():
+            name = userform.cleaned_data['name']
+            return HttpResponse(f'<h2>Hello, {name}</h2>')
+        else:
+            return HttpResponse('Invalid data')
     else:
         userform = UserForm()
-        return render(request, "crud/index.html", {"form": userform})
+        return render(request, 'crud/index.html', {'form': userform})
