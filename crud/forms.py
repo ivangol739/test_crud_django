58ffd3a032e7ec44d0b5eef461b49308aa547bb2
ivangol739 <ivangol739@gmail.com>
2025-02-02 20:54:57 +0300

    add system comments

--- crud/forms.py
+++ crud/forms.py
@@ -1,6 +1,12 @@
 from django.forms import Form, CharField, IntegerField
 
+from crud.models import Person
+
 
 class PersonForm(Form):
     name = CharField(label='Введите имя')
     age = IntegerField(label='Введите возраст')
+
+    class Meta:
+        model = Person
+        fields = ['name', 'age']
